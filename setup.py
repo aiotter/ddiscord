170ee8903db8d2900c28b4e34ca6887e8123aebd
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-06-08 16:19:35 +0900

    Add support python versions between 3.8 and 3.10

--- setup.py
+++ setup.py
@@ -29,5 +29,8 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
