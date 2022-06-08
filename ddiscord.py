0218f2f3f85441593054abd6795b06054bac8daf
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-06-08 16:09:53 +0900

    Update for discord.py v2.0
    * Using argparse for arguments parsing.
    * Add --intents parameter to custome intents.

--- ddiscord.py
+++ ddiscord.py
@@ -2,11 +2,11 @@ import discord
 import asyncio
 import textwrap
 import traceback
-import sys
 import io
 import os
 import platform
 import re
+import argparse
 from code import compile_command
 from contextlib import redirect_stdout
 from pathlib import Path
@@ -18,9 +18,17 @@ except ImportError:
     pass
 
 
+parser = argparse.ArgumentParser(prog="ddiscord")
+parser.add_argument("token", nargs="?", default="-", help="Login token")
+parser.add_argument(
+    "-i", "--intents", nargs="*", default=["default"]
+    help="Using intents('all' for all intents). If none, using Intents.default")
+args = parser.parse_args()
+
+
 async def run_debugger(client: discord.Client):
     print('Connecting to discord...', end='', flush=True)
-    await client.wait_until_ready()
+    await client.loop.run_in_executor(None, client.wait_until_ready())
     print(f'\rLogged in as {client.user} ({client.user.id})')
     print('You can refer to your Client instance as `client` variable. '
           'i.e. client.guilds', end='\n\n')
@@ -75,17 +83,11 @@ async def run_debugger(client: discord.Client):
 
 
 def get_token():
-    token = None
+    token = args.token
     token_path = Path('./token')
     token_env = os.environ.get('DISCORD_TOKEN')
 
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '-':
-            token = input()
-        else:
-            token = sys.argv[1]
-
-    elif token_path.exists():
+    if token_path.exists():
         token = token_path.read_text().rstrip()
 
     elif token_env:
@@ -100,16 +102,54 @@ def get_token():
     else:
         print('Send EOF to exit.')
 
-    if not token:
+    if token == "-":
         token = input('Input your token: ')
 
     return token
 
 
+def get_intents():
+    intents_path = Path('./intents')
+    intents_env = os.environ.get('DISCORD_INTENTS')
+
+    if intents_path.exists():
+        inte = intents_path.read_text().rstrip()
+    elif intents_env:
+        inte = intents_env
+
+    if args.intents == ["default"]:
+        return discord.Intents.default()
+    elif args.intents in [["all"], ["All"]]:
+        return discord.Intents.all()
+    elif args.intents in [["none"], ["None"]]:
+        return discord.Intents.none()
+    else:
+        inte = args.intents
+
+    intents = discord.Intents()
+    for i in inte:
+        parsed = i.replace(" ", "").split("=")
+        if not hasattr(intents, parsed[0]):
+            raise ValueError("Invalid argument has passed.")
+        setattr(intents, parsed[0], bool(parsed[1]))
+
+    return intents
+
+
 def main():
     token = get_token()
-    client = discord.Client()
-    client.loop.create_task(run_debugger(client))
+
+    if discord.version_info[0] < 2:
+        # discordのバージョンは1.7以下
+        client = discord.Client()
+        client.loop.create_task(run_debugger(client))
+    else:
+        # d.py2.0
+        client = discord.Client(intents=get_intents())
+        async def _setup_hook(self):
+            await run_debugger(self)
+        client.setup_hook = _setup_hook
+
     client.run(token)
 
 
